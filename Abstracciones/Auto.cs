a025acb9fa9c912b43ed6d1be13982bc5e57a37b
Leo <lancomputers@hotmail.com>
2019-10-16 21:44:11 -0300

    Primer Snapshot proyecto practica c#

---
+++ Abstracciones/Auto.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones
+{
+    class Auto
+    {
+        private int _anio;
+        private string _marca;
+        private string _modelo;
+        private string _color;
+        private string _patente;
+        private string _detalles;
+
+        //  Ejemplo Constructor
+
+        public Auto(int anio, string marca, string modelo, string color, string patente)
+        {
+            _anio = anio;
+            _marca = marca;
+            _modelo = modelo;
+            _color = color;
+            _patente = patente;
+        }
+
+        public int Anio
+        {
+            get { return _anio; }
+            set { _anio = value;}
+        }
+        public string Marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        public string Patente
+        {
+            get { return _patente; }
+            set { _patente = value; }
+        }
+
+        public string get_resumen()
+        {
+            mostrar_resumen();
+            return _detalles; 
+        }
+        public void mostrar_resumen()
+        {
+            _detalles = "Marca: "+_marca +"Modelo: " + _modelo + "Año Fabricacion: " + _anio + "Color: " + _color + "Patente: " + _patente;
+        }
+
+       public void Encender()
+        {
+            Console.WriteLine("Encender");
+        }
+        public void Mover_metros()
+        {
+            Console.WriteLine("Se mueve");
+        }
+        public void Apagar()        
+        {
+            Console.WriteLine("Apagar");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Abstracciones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto objauto = new Auto(1987,"Ford","Falcon","Bordo","VTP 717");
+              string resumen;
+            //objauto.Marca = "Ford";
+            //objauto.Modelo = "Falcon";
+            //objauto.Anio = 1987;
+            //objauto.Patente = "VTP 717";
+
+             resumen =   objauto.get_resumen();
+            objauto.Encender();
+            objauto.Mover_metros();
+            objauto.Apagar();
+
+
+            Console.WriteLine(resumen);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
